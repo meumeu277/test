a49a778d6791763b01436205febb3476cdfdc3c2
meumeu277 <78445569+meumeu277@users.noreply.github.com>
2021-02-04 17:46:11 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+                <link rel="stylesheet" href="css/style.css">
+
+
+        <title></title>
+    </head>
+    <body>
+        <div class="container">
+  
+  <h1>- SERVO -</h1>
+  
+  <div class="bird-container bird-container--one">
+    <div class="bird bird--one"></div>
+  </div>
+  
+  <div class="bird-container bird-container--two">
+    <div class="bird bird--two"></div>
+  </div>
+  
+  <div class="bird-container bird-container--three">
+    <div class="bird bird--three"></div>
+  </div>
+  
+  <div class="bird-container bird-container--four">
+    <div class="bird bird--four"></div>
+  </div>
+  
+<!--        <img src="img/moutain_right.png" class="img-fluid float-right" alt="...">-->
+
+	
+</div>
+        <div class="moutain">
+                        <img align="left" src="img/moutain_left.png" style="margin-top: -402px; z-index: 1; width: 750px">
+
+            <img align="right" src="img/moutain_right.png" style="margin-top: -411px; z-index: 1; width: 750px">
+      
+        </div>
+
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
